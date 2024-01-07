378f96d927d8c6e30873cd4acbee406bc5c315d2
Rishavjha45 <140633847+Rishavjha45@users.noreply.github.com>
2024-01-07 12:17:29 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const account_Id = 144553
+let account_Add = "Jaipur"
+let account_Type = "Private"
+let account_State = "Rajsthan"
+
+
+console.table([account_Id, account_Add, account_Type, account_State])
